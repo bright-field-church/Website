3aa3a23f03fc27add297efb30b100e246b4bedc6
Ricky Laskowski <Richard.lee.laskowski@gmail.com>
2018-05-21 16:06:03 -0500

    Standard Mobile Navigation Bar with Drop down

--- script/index.js
+++ script/index.js
@@ -4,37 +4,66 @@ $(document).ready(function()
     var accordion = document.getElementsByClassName("accordion");
     var i;
 
-    // This statement adds a click event listener that drops down a menu when the menu icon is clicked.
-    navbar_menu_icon_accordion.addEventListener("click", function()
-    {   
-        var navbar_menu_content_panel = this.nextElementSibling;
-
-        if(navbar_menu_content_panel.style.maxHeight)
+    for(i = 0; i < accordion.length; i++)
+    {
+        accordion[i].addEventListener("click", function()
         {
-            navbar_menu_content_panel.style.maxHeight = null;
+           var panel = this.nextElementSibling;
+           if(panel.style.display === "block")
+           {
+               panel.style.display = "none";
+           } 
+           else
+           {
+               panel.style.display = "block";
+           }
+        });
+    }
+
+    navbar_menu_icon_accordion.addEventListener("click", function()
+    {
+        var panel = this.nextElementSibling;
+        if(panel.style.display === "block")
+        {
+            panel.style.display = "none";
         }
         else
         {
-            navbar_menu_content_panel.style.maxHeight = navbar_menu_content_panel.scrollHeight + "px";
+            panel.style.display = "block";
         }
     });
 
-    //alert(accordion.length);
-    for(i = 0; i < accordion.length; i++)
-    { 
-        accordion[i].addEventListener("click", function()
-        {
-            var panel = this.nextElementSibling;
+    // // This statement adds a click event listener that drops down a menu when the menu icon is clicked.
+    // navbar_menu_icon_accordion.addEventListener("click", function()
+    // {   
+    //     var navbar_menu_content_panel = this.nextElementSibling;
 
-            if(panel.style.maxHeight)
-            {
-                panel.style.maxHeight = null;
-            }
-            else
-            {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-        });
-    }
+    //     if(navbar_menu_content_panel.style.maxHeight)
+    //     {
+    //         navbar_menu_content_panel.style.maxHeight = null;
+    //     }
+    //     else
+    //     {
+    //         navbar_menu_content_panel.style.maxHeight = navbar_menu_content_panel.scrollHeight + "px";
+    //     }
+    // });
+
+    // //alert(accordion.length);
+    // for(i = 0; i < accordion.length; i++)
+    // { 
+    //     accordion[i].addEventListener("click", function()
+    //     {
+    //         var panel = this.nextElementSibling;
+
+    //         if(panel.style.maxHeight)
+    //         {
+    //             panel.style.maxHeight = null;
+    //         }
+    //         else
+    //         {
+    //             panel.style.maxHeight = panel.scrollHeight + "px";
+    //         }
+    //     });
+    // }
 
 });
